2b44e0152c494923441eb227814ce8d9a520179b
Alexander Kolov <me@alexkolov.com>
2015-04-09 16:53:28 +0200

    Add framework target

---
+++ Cloudinary/CloudinaryFramework.h
@@ -0,0 +1,19 @@
+//
+//  CloudinaryFramework.h
+//  CloudinaryFramework
+//
+//  Created by Alexander Kolov on 9.4.15.
+//  Copyright (c) 2015 Cloudinary Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CloudinaryFramework.
+FOUNDATION_EXPORT double CloudinaryFrameworkVersionNumber;
+
+//! Project version string for CloudinaryFramework.
+FOUNDATION_EXPORT const unsigned char CloudinaryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudinaryFramework/PublicHeader.h>
+
+#import <CloudinaryFramework/Cloudinary.h>
